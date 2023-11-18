37ba5f6399050e66133bca985c82401885d73c1f
Xondamir <abdulazizovxondamirxon@gmail.com>
2023-11-18 11:04:03 +0500

    fd

--- Js/lang.js
+++ Js/lang.js
@@ -1,90 +1,43 @@
 const langArr = {
-
     "cheeseLang":{
         "uz":"pishloq ",
         "en":"cheese",
         "ru":"сыр",
     },
-    "cheese":{
-        "uz":"Pishloq - 24 569,04 so'm ",
-        "en":"Cheese - $2",
-        "ru":"Cыр - 177,93 ₽",
-
+    "meatLang":{
+        "uz":"go'sht",
+        "en":"meat",
+        "ru":"мясо",
     },
-    "meat":{
-        "uz":"Go'sht - 61 400,99 so'm",
-        "en":"Meat - $5",
-        "ru":"Mясо - 444,73 ₽",
-    },
-    "onion":{
-        "uz":"Piyoz - 12 281 so'm",
-        "en":"Onion - $1",
-        "ru":"лук - 88,79 ₽",
-    },
-    "salad":{
-        "uz":"Salat - 12 281 so'm",
-        "en":"Salad - $1",
-        "ru":"Cалат - 88,79 ₽",
-    },
-    "tomato":{
-        "uz":"Pomidor - 24 569,04 so'm",
-        "en":"Tomato - $2",
-        "ru":"помидор - 177,93 ₽",
-    },
-    "pickle":{
-        "uz":"Bodring - 36 845,99 so'm",
-        "en":"Pickle - $3",
-        "ru":"Oгурец - 266,84 ₽",
-    },
-    "bread":{
-        "uz":"Non - 24 569,04 so'm",
-        "en":"Bread - $2",
-        "ru":"Xлеб - 177,93 ₽",
-    },
-    "title":{
-        "uz":"Burger buyurtma",
-        "en":"Burger order",
-        "ru":"заказ бургера",
-    },
-    "Cheese":{
-        "uz":"Pishloq",
-        "en":"Cheese",
-        "ru":"Cыр",
-    },
-    "Meat":{
-        "uz":"Go'sht",
-        "en":"Meat",
-        "ru":"Mясо",
-    },
-    "Onion":{
-        "uz":"Piyoz",
-        "en":"Onion",
+    "onionLang":{
+        "uz":"piyoz",
+        "en":"onion",
         "ru":"лук",
     },
-    "Salad":{
-        "uz":"Salat",
-        "en":"Salad",
-        "ru":"Cалат",
+    "saladLang":{
+        "uz":"salat",
+        "en":"salad",
+        "ru":"салат",
     },
-    "Tomato":{
-        "uz":"Pomidor",
-        "en":"Tomato",
+    "tomatoLang":{
+        "uz":"pomidor",
+        "en":"tomato",
         "ru":"помидор",
     },
-    "Pickle":{
-        "uz":"Bodring",
-        "en":"Pickle",
-        "ru":"Oгурец",
+    "pickleLang":{
+        "uz":"bodring",
+        "en":"pickle",
+        "ru":"огурец ",
     },
-    "Retry":{
-        "uz":"Takroran",
-        "en":"Retry",
-        "ru":"Повторить",
+    "breadLang":{
+        "uz":"non",
+        "en":"bread",
+        "ru":"хлеб",
     },
-    "Send":{
-        "uz":"Jo'natish",
-        "en":"Send",
-        "ru":"Отправлять",
+    "titleLang":{
+        "uz":"burger buyurtma",
+        "en":"burger order",
+        "ru":"заказ бургера",
     },
 
 }
